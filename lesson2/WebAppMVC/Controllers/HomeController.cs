e40bab326f6d2aec20f57ef9aa0de8742e757041
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-02-11 10:00:48 +0200

    MVC: controllers, actions

---
+++ lesson2/WebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Hello, World!";
+        }
+
+        [NonAction]
+        public int Calc()
+        {
+            return 7;
+        }
+    }
+}
