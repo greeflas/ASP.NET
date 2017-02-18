fa03b5ef918fca4ad0fc7f247a091e0ca637c843
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-02-18 11:52:53 +0200

    MVC: model

---
+++ lesson3/WebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult GetProduct()
+        {
+            return View(new Product() {
+                id = 1,
+                Category = "Phones",
+                Name = "iPhone SE",
+                Description = "Cool phone for your lify style",
+                Price = 11000
+            });
+        }
+    }
+}
