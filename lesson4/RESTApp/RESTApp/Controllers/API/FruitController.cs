d4150ff5a0a55b7cbeb3f4505aee7dbba5584225
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2017-03-18 10:29:44 +0200

    REST

---
+++ lesson4/RESTApp/RESTApp/Controllers/API/FruitController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System;
+
+namespace RESTApp.Controllers.API
+{
+    public class FruitController : ApiController
+    {
+        static List<string> _fruits = new List<string>()
+        {
+            "Orange",
+            "Apple",
+            "Lime"
+        };
+
+        public List<string> GetAll()
+        {
+            return _fruits;
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            if (id > _fruits.Count || id < 0)
+            {
+                // Status code 404
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Fruit not found");
+            }
+
+            // Status code 200
+            return Request.CreateErrorResponse(HttpStatusCode.OK, _fruits[id]);
+        }
+
+        public HttpResponseMessage Post(string fruit)
+        {
+            _fruits.Add(fruit);
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, fruit);
+            response.Headers.Location = new Uri(Request.RequestUri + "/" + (_fruits.Count - 1));
+
+            return response;
+        }
+    }
+}
---
+++ lesson4/RESTApp/RESTApp/Controllers/API/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace RESTApp.Controllers.API
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public string GetDate()
+        {
+            return "Current date: " + DateTime.Now.ToString();
+        }
+    }
+}
